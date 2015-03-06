c8a246542338639b8e61a2c5bf4c467e34618ba1
vagrant <vagrant@precise32.(none)>
2015-03-06 20:58:08 +0000

    Add python mysql module.

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#apt-get update
-# apt-get install python-sqlalchemy || { echo 'installing python-sqlalchemy failed'; exit 1; }
+
+
+#apt-get update;
 
 function set_git_config_commit_vim {
   git config --global core.editor "vim"
@@ -11,15 +12,38 @@ function install {
   apt-get -y install $1 || { echo "ERROR: installing ${1}"; exit 1; }
 }
 
+function install_the_database {
+  install python-mysqldb
+
+  DB_REPO="http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu";
+
+  install python-software-properties;
+
+  apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db || { echo 'ERROR: adding keys for MariaDB'; exit 1; }
+echo $DB_REPO
+  add-apt-repository "deb $DB_REPO precise main" || { echo 'ERROR: adding repo for MariaDB'; exit 1; }
+  apt-get update;
+
+
+  install mariadb-server;
+}
 
 
 
-install 'vim';
-set_git_config_commit_vim;
 
-install 'shunit2';
+# install 'vim';
 
-install 'python-sqlalchemy' ;
+#set_git_config_commit_vim;
+
+#install 'shunit2';
+
+#install 'python-sqlalchemy' ;
+
+install_the_database;
+
+
+
+
 
 
 
